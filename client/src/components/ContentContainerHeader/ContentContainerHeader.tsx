ba7b5dc1c7cd1907e86b2834f0665598e68c6336
Deondre <deondre.garcia@yahoo.com>
2022-09-02 20:51:27 -0700

    pre-layout change

--- client/src/components/ContentContainerHeader/ContentContainerHeader.tsx
+++ client/src/components/ContentContainerHeader/ContentContainerHeader.tsx
@@ -4,7 +4,7 @@ import "./ContentContainerHeader.css";
 const ContentContainerHeader = ({ category }: { category: string }) => {
   return (
     <div className="content-container-header-container">
-      <h1>{category}</h1>
+      <h1 className="content-container-header-text">{category}</h1>
     </div>
   );
 };
